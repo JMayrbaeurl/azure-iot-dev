926685c379c12ea1a8f119a84ccdbb991ebd6ee1
Jürgen Mayrbäurl <jmayrbaeurl@live.at>
2017-03-18 09:54:50 -0700

    Added missing files and folders for basic integration testing

---
+++ idmgmt/azure-iot-idmgmt-it/src/main/java/com/microsoft/jm/azure/sdk/iot/idmgmt/DeviceIdentityManagementIT.java
@@ -0,0 +1,9 @@
+package com.microsoft.jm.azure.sdk.iot.idmgmt;
+
+public class DeviceIdentityManagementIT {
+
+	public DeviceIdentityManagementIT() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
